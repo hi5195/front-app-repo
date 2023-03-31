06f2c6dc9595147a2871a2921c8533cbfab38d92
hi5195 <hi5195@naver.com>
2023-03-31 00:38:53 +0000

    4 change

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App(props) {
   const [query, setQuery] = useState('eks');
   const [search, setSearch] = useState('eks');
 
-  var url = 'http://k8s-eksdemogroup-e0353f9ab7-111336850.ap-northeast-2.elb.amazonaws.com/contents/${search}'
+  var url = `http://k8s-eksdemogroup-e0353f9ab7-111336850.ap-northeast-2.elb.amazonaws.com/contents/${search}`
   
   useEffect(() => {
     const fetchData = async () => {
